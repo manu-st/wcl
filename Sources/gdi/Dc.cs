3ebd58c75d1cc712694ef1f4ad9f80938b1dbed7
Emmanuel Stapf <manu-silicon@users.noreply.github.com>
2015-09-16 18:08:09 +0900

    Added missing new classes that wraps the HDC structure to draw using the WM_PAINT message.

---
+++ Sources/gdi/Dc.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Diagnostics.Contracts;
+using WCL.Structs;
+using WCL.Windows;
+
+namespace WCL.Gdi
+{
+	public abstract class Dc
+	{
+
+#region Basic operations
+		public abstract void get ();
+			// Get the device context.
+
+		public abstract void release ();
+		// Release the device context.
+		#endregion
+
+
+#region Drawing operations
+
+		public void draw_text(string s, int x, int y)
+		{
+			Win32.TextOut(_item, x, y, s, s.Length);
+		}
+
+		public void draw_rectangle (int left, int top, int right, int bottom)
+		{
+			Win32.Rectangle (_item, left, top, right, bottom);
+		}
+
+		public void draw_rectangle (Rect r)
+		{
+			draw_rectangle (r.left, r.top, r.right, r.bottom);
+		}
+
+
+#endregion
+
+#region Implementation: Access
+
+		protected IntPtr _item;
+
+#endregion
+	}
+}
---
+++ Sources/gdi/PaintDc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using WCL.Structs;
+using WCL.Windows;
+
+namespace WCL.Gdi
+{
+	public class PaintDc : Dc
+	{
+#region Initialization
+		public PaintDc (Window a_window)
+		{
+			Contract.Requires (a_window != null, "Window not null");
+			Contract.Requires (a_window.exists (), "Window exists");
+
+			window = a_window;
+			_item = IntPtr.Zero;
+
+			Contract.Ensures (window == a_window, "window set");
+			Contract.Ensures (_item == IntPtr.Zero, "item set");
+		}
+#endregion
+
+#region Access
+		public PaintStruct paint_struct
+		{
+			get { return _paint_struct; }
+		}
+
+		public Rect paint_rect ()
+		{
+			return _paint_struct.rcPaint;
+		}
+#endregion
+
+#region Basic operations
+		public override void get ()
+		{
+			_item = Win32.BeginPaint(window.item, out _paint_struct);
+		}
+
+		public override void release ()
+		{
+			Win32.EndPaint(window.item, ref _paint_struct);
+		}
+#endregion
+
+#region Implementation: Access
+		protected PaintStruct _paint_struct;
+		protected Window window;
+#endregion
+
+	}
+}
